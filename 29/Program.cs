4b09bbd9219172143335b56ec6aec3a13177b5b7
alecs000 <alecs.2004fed@gmail.com>
2021-10-19 23:39:44 +0300

    Добавьте файлы проекта.

---
+++ 29/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace _29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = " Тук Тук  -Кто это?  -Это я, твоя дверь  -Аа да  -А я думала Сова  -Индюк тоже думал и в щи попал";
+
+            while (true)
+            {
+               
+                for (int i = 0; i < 71; i++)
+                {
+                    string s = str.Substring(i, 20);
+                    Console.WriteLine(s);
+                    Thread.Sleep(60);
+                    Console.SetCursorPosition(0, 0);
+
+                }
+            }
+           
+                
+        }
+    }
+}
